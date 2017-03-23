53aa352f69c33257924b399af79931edb3c74864
shatalovdm <innoshatalov@gmail.com>
2017-03-24 00:17:48 +0300

    Set up Gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,33 @@
+'use strict';
+
 var gulp = require('gulp');
-var uglifycss = require('gulp-uglifycss');
- 
-gulp.task('css', function () {
-  gulp.src('./src/*.css')
-    .pipe(uglifycss({
-      "maxLineLen": 80,
-      "uglyComments": true
-    }))
-    .pipe(gulp.dest('./dist/'));
+var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+
+gulp.task('workflow', function () {
+    gulp.src('./src/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cssnano({
+            zindex: false
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(rename(function (path) {
+            if(path.extname === '.css') {
+                path.basename += '.min';
+            }
+        }))
+        .pipe(gulp.dest('./dist/'))
+});
+
+//Watch task
+gulp.task('default', function () {
+  gulp.watch('./src/**/*.scss', ['workflow']);
 });
